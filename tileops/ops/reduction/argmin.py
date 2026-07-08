944eced8e717c1af87c507b001a8ce9147564bca
JianXin Wang <wjxshanxi@163.com>
2026-07-08 20:22:29 +0800

    [Fix] Fix tests/ops/test_argreduce.py by adding corresponding maca backend functions

--- tileops/ops/reduction/argmin.py
+++ tileops/ops/reduction/argmin.py
@@ -11,6 +11,8 @@ import torch
 
 from tileops.kernels.kernel_base import Kernel
 from tileops.kernels.reduction.argreduce import ArgreduceKernel
+from tileops.kernels.reduction.argreduce_maca import ArgreduceMACAKernel
+from tileops.utils import is_maca
 
 from .reduce import _ReduceOpBase
 
@@ -37,7 +39,7 @@ class ArgminFwdOp(_ReduceOpBase):
 
     _op_kind = "argmin"
     _kernel_key = "argreduce"
-    _kernel_cls = ArgreduceKernel
+    _kernel_cls = ArgreduceMACAKernel if is_maca() else ArgreduceKernel
 
     def __init__(
         self,
